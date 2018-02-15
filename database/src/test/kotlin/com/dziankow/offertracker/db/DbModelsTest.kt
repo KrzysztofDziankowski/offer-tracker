678c54c246a2f22a46ea39dd96715d603e926ac8
Krzysztof Dziankowski <krzysztof.dziankowski@nokia.com>
2018-02-15 23:00:37 +0100

    Add slf4j and log4j loggers

--- database/src/test/kotlin/com/dziankow/offertracker/db/DbModelsTest.kt
+++ database/src/test/kotlin/com/dziankow/offertracker/db/DbModelsTest.kt
@@ -4,10 +4,13 @@ import com.dziankow.offertracker.db.model.Offer
 import com.dziankow.offertracker.db.model.Seller
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import org.slf4j.LoggerFactory
+
 
 class DbModelsTest() {
 
-    private val entityManagerUtil: EntityManagerUtil = EntityManagerUtil()
+    private val logger = LoggerFactory.getLogger(DbModelsTest::class.java)
+    private val entityManagerUtil: EntityManagerUtil = EntityManagerUtil("test")
 
     fun getSimpleOffer(offerName: String, sellerName: String): Offer {
         val parameters = HashMap<String, String>()
@@ -33,16 +36,16 @@ class DbModelsTest() {
     @Test
     fun offerModelTest() {
 
-        println("After Sucessfully insertion ")
+        logger.info("After Sucessfully insertion ")
         val offer1 = entityManagerUtil.saveEntity(getSimpleOffer("offerName1", "sellerName1"))
         val offer2 = entityManagerUtil.saveEntity(getSimpleOffer("offerName2", "sellerName2"))
         assertEquals(listOf(offer1, offer2), entityManagerUtil.listEntities(Offer::class.java))
 
-        println("After Sucessfully deletion ")
+        logger.info("After Sucessfully deletion ")
         entityManagerUtil.deleteEntity(offer1.id, Offer::class.java)
         assertEquals(listOf(offer2), entityManagerUtil.listEntities(Offer::class.java))
 
-        println("After Sucessfully modification ")
+        logger.info("After Sucessfully modification ")
         offer2.name = "updated"
         entityManagerUtil.saveEntity(offer2)
         assertEquals(listOf(offer2), entityManagerUtil.listEntities(Offer::class.java))
